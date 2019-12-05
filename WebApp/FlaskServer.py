40da210c6117bc2256198b53dc8f5d799b2144c9
seanmoylan <sean.cheezy.moylan@gmail.com>
2019-12-05 10:13:48 +0000

    Having issues getting keras to work in flask server

--- WebApp/FlaskServer.py
+++ WebApp/FlaskServer.py
@@ -10,6 +10,7 @@ import base64
 import logging
 from PIL import ImageOps, Image
 from flask import render_template
+from keras.models import model_from_json
 
 
 HEIGHT, WIDTH = 28,28
@@ -31,38 +32,49 @@ def home():
 def uploadimage():
   # Get the image from the request.
   theimage = fl.request.values['imageData']
-  
+
   # Print to the console.
   print(theimage)
 
-  
-
-
   # Decode the string to an image, from byte 22 on
   decodedimage = base64.b64decode(theimage[22:])
-  
 
   # Try to save the image
   with open ("drawing.png", "wb") as f:
   	f.write(decodedimage)
   
+  model = load_model()
   # resize image so it can be tested against the model
-  resize_image()
+  image_to_predict = resize_image()
 
-  predict()
+
+  prediction_array = model.predict(image_to_predict)
+  prediction = np.argmax(prediction_array)
+  
   # Return a response
-  return decodedimage
+  return {"prediction": prediction}
 
 
 def resize_image():
     # Adapted from: https://dev.to/preslavrachev/python-resizing-and-fitting-an-image-to-an-exact-size-13ic
-    img = Image.open('drawing.png')
-    size = (28, 28)
-    img = ImageOps.fit(img, DIMENTIONS, Image.ANTIALIAS)
-    img.save('drawing2.png')
+  original_image = Image.open('drawing.png').convert("L")
+  size = (28,28)
+  original_image = ImageOps.fit(original_image, size, Image.ANTIALIAS)
 
-def predict():
+  img_array = np.array(original_image).reshape(1,28,28,1)
+  return img_array
+
+def load_model():
   # import model
-  # get image after it is resized
-  # run the prediction and return the digit it gives
-  return 5
+  json_model = open('model.json','r')
+  load_model_json = json_model.read()
+  json_model.close()
+  loaded_model = model_from_json(load_model_json)
+
+  loaded_model.load_weights('model.h5')
+  return loaded_model
+
+def predict(model, image_array):
+  prediction_array = model.predict(image_array)
+  prediction = np.argmax(prediction_array)
+  return prediction
