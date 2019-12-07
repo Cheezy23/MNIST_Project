318afc2e109530cbe97fc99fdbb32edd1bfe35cc
seanmoylan <sean.cheezy.moylan@gmail.com>
2019-12-07 10:55:42 +0000

    Added some minor modifications to the server an notebook

--- WebApp/test.py
+++ WebApp/test.py
@@ -1,16 +1,12 @@
-# Adapted from: https://www.palletsprojects.com/p/flask/
-# https://www.youtube.com/watch?v=6Qs3wObeWwc
-# Run with: env FLASK_APP=random-wa.py flask run
-
-# Imports
+import json
 import flask as fl
 import numpy as np
 import base64
-import tensorflow as tf
 import logging
-from tensorflow import keras
+import keras.models as model_from_json
 from PIL import ImageOps, Image
 from flask import render_template
+#from keras.models import model_from_json
 
 
 HEIGHT, WIDTH = 28,28
@@ -58,7 +54,7 @@ def uploadimage():
 def reshape_image():
     # Adapted from: https://dev.to/preslavrachev/python-resizing-and-fitting-an-image-to-an-exact-size-13ic
   original_image = Image.open('drawing.png').convert("L")
-  original_image = ImageOps.fit(original_image, DIMENTIONS, Image.ANTIALIAS)
+  original_image = ImageOps.fit(original_image, WIDTH, HEIGHT, Image.ANTIALIAS)
 
   img_array = np.array(original_image).reshape(1,WIDTH,HEIGHT,1)
   return img_array
@@ -68,7 +64,7 @@ def load_model():
   json_model = open('model/model.json','r')
   load_model_json = json_model.read()
   json_model.close()
-  loaded_model = keras.models.model_from_json(load_model_json)
+  loaded_model = model_from_json(load_model_json)
 
   loaded_model.load_weights('model/model.h5')
   return loaded_model
@@ -77,5 +73,3 @@ def predict(model, image_array):
   prediction_array = model.predict(image_array)
   prediction = np.argmax(prediction_array)
   return prediction
-
-#app.run(debug=True)
